2f7c9d389feb7d9bd0c1d99f10f1c48db0d92594
Pedro <pedrocicoleme@gmail.com>
2016-04-19 23:32:20 -0300

    sumList

--- homework-1/main.hs
+++ homework-1/main.hs
@@ -7,6 +7,16 @@ doubleEverySecond :: [Int] -> [Int]
 doubleEverySecond [] = []
 doubleEverySecond (x:y:ys) = (x*2) : y : doubleEverySecond ys
 
+-- Add the digits of the doubled values and the undoubled digits
+-- from the original number. For example, [2,3,16,6] becomes
+-- 2+3+1+6+6 = 18.
+
+-- sumList :: (Num a) => [a] -> a
+-- sumList [] = 0
+-- sumList (x:xs) = (+) x (sumList xs)
+
+sumList :: (Num a) => [a] -> a
+sumList xs = foldr (+) 0 xs
+
 main :: IO ()
-main = do
-    print (doubleEverySecond ([1,1,1,1,1,1]))
+main = print (sumList(doubleEverySecond [1,1,1,1,1,1]))
